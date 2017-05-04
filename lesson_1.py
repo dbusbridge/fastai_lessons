dfa621ac718702fdf28c2dd65c6ad0d8e2e303ad
Dan Busbridge <daniel.busbridge@quintilesims.com>
2017-05-04 12:50:36 +0100

    Added skl dependency

--- lesson_1.py
+++ lesson_1.py
@@ -1,11 +1,17 @@
 # Imports #####################################################################
 import json
 import os
+import utils
+
+import vgg16
 
 import numpy as np
 import importlib as il
 
 from matplotlib import pyplot as plt
+from utils import plots
+
+from vgg16 import Vgg16
 
 
 # Display options #############################################################
@@ -16,10 +22,17 @@ np.set_printoptions(precision=4, linewidth=100)
 # Kaggle competition https://www.kaggle.com/c/dogs-vs-cats
 path = "data/dogscats/"
 
-import utils; il.reload(utils)
-from utils import plots
+# Set the batch size
+batch_size = 64
 
 
-# As large as you can, but no larger than 64 is recommended.
-# If you have an older or cheaper GPU, you'll run out of memory, so will have to decrease this.
-batch_size=64
+# Training ####################################################################
+# Create an instance of the Vgg model
+vgg = Vgg16()
+
+# Grab a few images at a time for training and validation.
+# NB: They must be in subdirectories named based on their category
+batches = vgg.get_batches(path+'train', batch_size=batch_size)
+val_batches = vgg.get_batches(path+'valid', batch_size=batch_size*2)
+vgg.finetune(batches)
+vgg.fit(batches, val_batches, nb_epoch=1)
