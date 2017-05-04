177d15266ff4305a372128e97781a120856fcb2d
Dan Busbridge <daniel.busbridge@quintilesims.com>
2017-05-04 12:45:42 +0100

    Fixed PIL dependeny with image -> pillow

--- lesson_1.py
+++ lesson_1.py
@@ -13,9 +13,13 @@ np.set_printoptions(precision=4, linewidth=100)
 
 
 # Code ########################################################################
-
-# Cats vs dogs
+# Kaggle competition https://www.kaggle.com/c/dogs-vs-cats
 path = "data/dogscats/"
 
 import utils; il.reload(utils)
 from utils import plots
+
+
+# As large as you can, but no larger than 64 is recommended.
+# If you have an older or cheaper GPU, you'll run out of memory, so will have to decrease this.
+batch_size=64
